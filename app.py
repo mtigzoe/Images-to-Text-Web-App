e273106a05044187dd0db9f182d13cf5fc070a6f
mtigzoe <mloznea@g.clemson.edu>
2021-11-04 16:07:09 -0400

    Make changes to files

--- app.py
+++ app.py
@@ -2,30 +2,25 @@ import pytesseract
 from PIL import Image      
 
 import os
-from flask import Flask, flash, request, redirect, render_template
+from flask import Flask, flash, request, redirect, render_template, send_from_directory
+
 from werkzeug.utils import secure_filename
 
 app=Flask(__name__)
 
-app.secret_key = "secret key"
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+UPLOAD_FOLDER ='static/uploads/'
 
-# Get current path
-path = os.getcwd()
-# file Upload
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
-pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'   
-        
-
-
-# Make directory if uploads is not exists
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.mkdir(UPLOAD_FOLDER)
-
+app.config['SECRET_KEY'] = 'opencv'  
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024
 
-# Allowed extension you can set your own
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+#pytesseract.pytesseract.tesseract_cmd ='C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'   
+#pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/share/tesseract-ocr/4.00/tessdata'
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 def allowed_file(filename):
@@ -35,7 +30,6 @@ def allowed_file(filename):
 @app.route('/')
 def upload_form():
     return render_template('upload.html')
-
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -55,17 +49,17 @@ def upload_file():
         for file in files:
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
                 
-                extracted_text = pytesseract.image_to_string(Image.open(filename))
+                image_scan = Image.open(file)
+                extracted_text = pytesseract.image_to_string(image_scan)
                 image_list.append(filename)
                 image_file_list.append(extracted_text)
         
         return render_template('upload.html',
                                    msg='Successfully processed',                                   
-                                   image_info= zip(image_file_list, image_list),
-                                   img_src=UPLOAD_FOLDER + file.filename)
+                                   image_info= zip(image_file_list, image_list))                               
     
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000,debug=False,threaded=True)
+    app.run(debug=True)
